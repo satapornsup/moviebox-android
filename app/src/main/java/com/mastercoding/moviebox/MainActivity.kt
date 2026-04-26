8271dbddd27392fb2b5982d5fa33e6ecb6d008b6
{sataporn.sup} <{sataporn.sup@gmail.com}>
2026-04-26 22:31:06 +0700

    [Add] Bootstrap a Hilt application.

--- app/src/main/java/com/mastercoding/moviebox/MainActivity.kt
+++ app/src/main/java/com/mastercoding/moviebox/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mastercoding.moviebox.ui.theme.MovieBoxTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +23,7 @@ class MainActivity : ComponentActivity() {
             MovieBoxTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "MovieBox Testing",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
---
+++ app/src/main/java/com/mastercoding/moviebox/MovieBoxApp.kt
@@ -0,0 +1,8 @@
+package com.mastercoding.moviebox
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieBoxApp : Application() {
+}
